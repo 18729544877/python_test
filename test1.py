3fcd9e16b3c65404c99953eafc0148e80ffe1fee
18729544877 <www_wanglei111@163.com>
2019-12-30 14:39:44 +0800

    add first commit by wall

---
+++ test1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+for i in range (1,9):
+    for j in range (1,9):
+        print(str(i) + '*' + str(j) + '=' + str(i * j))
+        print("hu li ben wu yu")
+        print("????= = = m vv ????")
+        print("as")
+        print("gulp")
+        print("bare")
+        print("move")
+        print("down")
+        print("here")
+        print("kill")
+        print("jump")
+        print("file down file file jere")
+
+
+
