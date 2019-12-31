a311f0471af48ab361f3079118d5bbb3014fcc45
18729544877 <www_wanglei111@163.com>
2019-12-31 11:56:57 +0800

    add new file

---
+++ test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+for i in range (1,9):
+    for j in range (1,9):
+        print(str(i) + '*' + str(j) + '=' + str(i * j))
+        print("hu li ben wu yu")
+        print("????= = = m vv ????")
+        print("as")
+
+
+
+
